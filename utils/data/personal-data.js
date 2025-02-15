e9d43bce22e8ae7ecdb758a9820f0c0a0160360f
Deep Kardani <kardanideep1@gmail.com>
2025-02-15 12:49:59 +0530

    update data

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -14,5 +14,5 @@ export const personalData = {
   stackOverflow: '/',
   leetcode: "/",
   devUsername: "",
-  resume: "https://drive.google.com/file/d/1a3iJP1dr8O1znZnkjRpFlHIS-vxcuM5S/view?usp=drive_link"
+  resume: "https://drive.google.com/file/d/1a9EF6LW1wlfZ7SkpA57yQ1HXNLJ-_4xy/view?usp=drive_link"
 }
